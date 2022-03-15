c70e2ceb01391bbe5845c9b6296221808977fd8c
V_V <khomechik@gmail.com>
2022-03-15 17:25:09 +0300

    dinamic_stairway

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -1,4 +1,5 @@
 from typing import Union, Sequence
+from itertools import islice
 
 
 def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
@@ -8,5 +9,13 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: list of ints, where each int is a cost of appropriate step
     :return: minimal cost of getting to the top
     """
-    print(stairway)
-    return 0
+    if len(stairway) == 0:
+        return -1
+    if len(stairway) == 1:
+        return stairway[0]
+
+    before_last_price, last_price = stairway[0], stairway[1]
+    for current_price in islice(stairway, 2, len(stairway)):
+        before_last_price, last_price = last_price, min(before_last_price, last_price) + current_price
+
+    return last_price
